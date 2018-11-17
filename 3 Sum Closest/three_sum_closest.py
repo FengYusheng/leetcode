d56d9eb81cfb206b820448c3184fce69ea8fa4ef
skywalker <maverick.fengys@gmail.com>
2018-11-17 16:25:11 +0800

    3 Sum Closest

---
+++ 3 Sum Closest/three_sum_closest.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import time
+import functools
+
+
+def time_it(func):
+    @functools.wraps(func)
+    def decorator(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        end = time.time()
+        print('{0} costs {1:' '>4.3f}s'.format(func.__name__, end-start))
+        return ret
+    return decorator
+
+
+
+@time_it
+def threeSumClosest(array, target):
+    return []
+
+
+
+if __name__ == '__main__':
+    array = [-1, 2, 1, -4]
+    expected = [-1, 1, 2]
+    target = 1
+    actual = threeSumClosest(array, target)
+    assert expected == actual, '{0} is expected, but the actual result is {1}.'.format(expected, actual)
