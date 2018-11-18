40b2b06233ad0aec7a6ea2dec2b785823d5e7a41
skywalker <maverick.fengys@gmail.com>
2018-11-18 20:34:33 +0800

    Letter Combinations of a Phone Number: a recursion implementation

--- Letter Combinations of a Phone Number/letterCombinations.py
+++ Letter Combinations of a Phone Number/letterCombinations.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # This is a problem about backtracing.
-# https://en.wikibooks.org/wiki/Algorithms/Backtracking
+# https://en.wikibooks.org/wiki/Algorithms/Backtracking(depth first search or branch and bound)
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/discuss/8063/Python-solution
 import time
 import functools
+from collections import deque
 
 
 def time_it(func):
@@ -11,26 +13,40 @@ def time_it(func):
         start = time.time()
         ret = func(*args, **kwargs)
         end = time.time()
+        print('{0} costs {1:' '>4.3f}s.'.format(func.__name__, end-start))
         return ret
     return decorator
 
 
+mapping = {
+    '0' : '',
+    '1' : '',
+    '2' : 'abc',
+    '3' : 'def',
+    '4' : 'ghi',
+    '5' : 'jkl',
+    '6' : 'mno',
+    '7' : 'pqrs',
+    '8' : 'tuv',
+    '9' : 'wxyz'
+}
+
+
+
 @time_it
 def letterCombinations(phone_num):
-    mapping = {
-        '0' : '',
-        '1' : '',
-        '2' : 'abc',
-        '3' : 'def',
-        '4' : 'ghi',
-        '5' : 'jkl',
-        '6' : 'mno',
-        '7' : 'pqrs',
-        '8' : 'tuv',
-        '9' : 'wxyz'
-    }
+    ret = []
 
-    return []
+    if len(phone_num) == 0:
+        ret = []
+    elif len(phone_num) == 1:
+        ret = [i for i in mapping[phone_num[0]]]
+        # ret = list(mapping[phone_num[0]])
+    elif len(phone_num) > 1:
+        prev = letterCombinations(phone_num[:-1])
+        ret = [i+j for i in prev for j in mapping[phone_num[-1]]]
+
+    return ret
 
 
 if __name__ == '__main__':
