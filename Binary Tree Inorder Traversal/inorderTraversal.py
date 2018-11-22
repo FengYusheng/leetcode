2e73c98e4d9856e61c35e078670fdaa67dec3ad6
skywalker <maverick.fengys@gmail.com>
2018-11-22 13:55:46 +0800

    Binary Tree Inorder Traversal

---
+++ Binary Tree Inorder Traversal/inorderTraversal.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import time
+import functools
+
+
+def time_it(func):
+    @functools.wraps(func)
+    def decorator(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        end = time.time()
+        print('{0} costs {1:' '>4.3f}s'.format(func.__name__, end-start))
+        return ret
+    return decorator
+
+
+@time_it
+def inorderTraversal(root):
+    '''
+    :type root: TreeNode
+    :rtype: List[int]
+    '''
+    return []
+
+
+if __name__ == '__main__':
+    expected = [1, 3, 2]
+    actual = inorderTraversal([])
+    assert expected == actual, '{0} is expected, but the actual result is {1}'.format(expected, actual)
