337bc86fdfce572486827554879b4756b6a59bc5
skywalker <maverick.fengys@gmail.com>
2018-12-17 22:47:05 +0800

    Recover Binary Search Tree: an dfs solution

--- Recover Binary Search Tree/recoverTree.py
+++ Recover Binary Search Tree/recoverTree.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
 import sys
 import os
 import time
@@ -7,6 +8,7 @@ import functools
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from base.tree.BinarySearchTree import TreeNode
 
+# prev = first = second = None
 
 def time_it(func):
     @functools.wraps(func)
@@ -14,7 +16,7 @@ def time_it(func):
         start = time.time()
         ret = func(*args, **kwargs)
         end = time.time()
-        print('{0} costs {1:' '>4:3f}s.'.format(func.__name__, (end-start)*1000))
+        print('{0} costs {1:' '>4.3f}s.'.format(func.__name__, (end-start)*1000))
         return ret
     return decorator
 
@@ -60,16 +62,40 @@ def recorverTree(root):
     '''
     I have spent too many hours to consider the cases which are beyond this problem itself.
     I should stop to focus on this problem itself. We can get a sorted array when we
-    in-order traverse a binary search tree. Given the description of the problem, two
+    in-order traverse a binary search tree. Given the description of the problem, only two
     elements among this array should be swapped. These tow elements are either
     larger than the elments after them or smaller than the elements before them.
 
-    Treat a binary search tree as a sorted arrary.
-    '''
-    return []
+    Treat a binary search tree as a sorted arrary. Every element should be smaller
+    than the elements before it.
+    3, 2, 1: swap 3(first) and 1(second)
+    1, 3, 2, 4: swap 3(first) and 2(second)
+    The "deep first traverse" method finds out the two error elements.
 
+    Note: I don't know why my submission failed when I implmented this method with global variables.
+    '''
+    def _dft(pivot, elements=[None, None, None]):
+        # global prev, first, second
+        if pivot:
+            _dft(pivot.left, elements)
+            if elements[0]:
+                if elements[0].val > pivot.val and not elements[1]:
+                    elements[1] = elements[0]
+                    elements[2] = pivot
+                elif elements[0].val > pivot.val:
+                    elements[2] = pivot
+
+            elements[0] = pivot
+            _dft(pivot.right, elements)
+
+    elements = [None, None, None]
+    _dft(root, elements)
+    if elements[1]:
+        elements[1].val, elements[2].val = elements[2].val, elements[1].val
 
 
 if __name__ == '__main__':
-    root = build([1,2,3,4,5])
-    print(dft(root))
+    root = build([1,2,3])
+    # print(dft(root))
+    recorverTree(root)
+    # print(dft(root, ret=[]))
